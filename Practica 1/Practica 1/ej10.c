eb8d366cca901142c6ee7a8e76592b2ed6bd2e8c
Enzo Stabile <133225300+EnzoStreet@users.noreply.github.com>
2025-04-03 21:06:24 -0300

    Add files via upload

---
+++ Practica 1/Practica 1/ej10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    for (int j = 10; j > 0; j--) {
+        printf("j = %d\n", j);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("i = %d\n", i);
+    }
+    return 0;
+}
